ebbf15d02bfae580152d401146a197bb72b14f85
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-03 16:14:35 -0600

    added fixed links to desktop view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import NavBar from "./components/navbar";
 import Home from "./components/Home";
+import { Links } from "./components/Links";
 
 function App() {
   return (
     <div>
     <NavBar/>
     <Home />
+
+    <Links />
     </div>
   );
 }
