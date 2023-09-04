d32009150613e9faede326431dace109ae72215b
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-04 10:06:50 -0600

    Added format of About section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import NavBar from "./components/navbar";
 import Home from "./components/Home";
 import { Links } from "./components/Links";
+import About from "./components/About";
 
 function App() {
   return (
     <div>
     <NavBar/>
     <Home />
+    <About />
 
     <Links />
     </div>
