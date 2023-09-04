d5216b22c511b0c38ee8160588ff523aaad7fe99
Juan Pablo Archuleta <pabloarchuleta@gmail.com>
2023-09-04 16:40:46 -0600

    Made form functional

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
           <p className='py-6'>submit the form below to get in touch 📧</p>
         </div>
         <div className='flex justify-center items-center'>
-          <form action="" className='flex flex-col w-full md:w-1/2'>
+          <form action="https://getform.io/f/4e7c1d6d-2380-449b-b0a5-18a9ef4de54c" method="POST"className='flex flex-col w-full md:w-1/2'>
             <input  type="text" name="name" placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
             <input  type="text" name="email" placeholder='Enter your email' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'/>
             <textarea name="message" placeholder='Enter your message' rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
