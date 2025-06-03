04872a859af8964901ac34349f8e5c8ef8154bbc
pabloarchuleta <pablo.archuleta@lcu.edu>
2025-06-03 10:10:51 -0500

    tsk file for main, header and footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
-function App() {
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./pages/Main";
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
