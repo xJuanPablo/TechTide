318399d42db85eab149a90178b6e30ef3d9e3c71
Pablo Archuleta <pabloarchuleta@gmail.com>
2025-06-08 21:51:06 -0500

    Created Card Component File

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card: React.FC = () => {
+    return (
+    <article>
+    <div>
+        <header>Header</header>
+        <img src="" alt="" />
+        <div>
+            <button>Learn More</button>
+        </div>
+    </div>
+    </article>
+    );
+};
+
+export default Card;
