ce6e781670036d6158cd3b8eedde7f583aaf3fcf
Pablo Archuleta <pabloarchuleta@gmail.com>
2025-06-08 22:41:32 -0500

    Created theme context

---
+++ client/src/context/ThemeContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useContext, useState, useEffe } from 'react';
+
+function ThemeContext() {
+    return (
+        <div>
+            ThemeContext
+        </div>
+    )
+}
+
+export default ThemeContext;
