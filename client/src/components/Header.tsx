a0c514bc059a8b669b8774e00e87a4b4e20a65f3
Pablo Archuleta <pabloarchuleta@gmail.com>
2025-06-11 13:39:23 -0500

    created basic header structure

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,13 +1,30 @@
 import { useTheme } from "../hooks/useTheme";
+import { NavLinks } from "../data/NavLinks";
 
 const Header: React.FC = () => {
   const { theme, toggleTheme } = useTheme()
 
   return (
-    <header>
-      <button onClick={toggleTheme}>
-      {theme === 'light' ? '🌙' : '🌤️'}
-      </button>
+    <header className="pt-4 pb-3 mb-3 shadow">
+      <div className="container">
+        <div className="d-flex justify-content-around">
+          <h1 className="mb-0">TechTide</h1>
+          <ul className="d-none d-md-flex list-unstyled mb-0 align-items-center">
+            {NavLinks.map((link, index) => (
+              <li key={index} className="mx-3">
+                <a href={link.url}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+            <li>
+              <button onClick={toggleTheme} className="border rounded bg-transparent">
+              {theme === 'light' ? '🌙' : '🌤️'}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </header>
   )
 }
