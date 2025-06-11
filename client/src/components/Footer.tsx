49b2ed99b82e83ecbadfbcc07ab3f8fe8da20429
Pablo Archuleta <pabloarchuleta@gmail.com>
2025-06-11 14:17:36 -0500

    Created basic footer structure

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
 const Footer: React.FC = () => {
   return (
-    <footer>
-      Footer
+    <footer className='border-top rounded-top-5'>
+      <div className='container'>
+        <h4 className='mt-4'>Speak Soon, Stay Lucky!</h4>
+        <div className='py-4 mb-3 mt-3 border rounded shadow'>
+          <div className="grid">
+            <div className="g-col-6">
+
+            </div>
+            <div className="g-col-6">
+              <div className="d-block">
+
+              </div>
+              <div className="d-block">
+                <p className="mb-1">
+                  Connect:
+                </p>
+                <ul className='d-flex list-unstyled'>
+                  <li className='me-3'>
+                    <a href="">LinkedIn</a>
+                  </li>
+                  <li>
+                    <a href="">GitHub</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='text-center'>
+          © 2025, TechTide
+        </div>
+      </div>
     </footer>
   )
 }
