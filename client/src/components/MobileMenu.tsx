bbf9a0509003b7e59d74ffe85b1f8a75e15bf325
Pablo Archuleta <pabloarchuleta@gmail.com>
2025-07-07 09:11:07 -0500

    Update mobile menu style

--- client/src/components/MobileMenu.tsx
+++ client/src/components/MobileMenu.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { NavLinks } from '../data/NavLinks';
 
 const MobileMenu: React.FC = () => {
   return (
-    <div>MobileMenu</div>
+    <nav id='mobileNav' className='h-100 d-flex justify-content-center align-items-center'>
+      <ul>
+        {NavLinks.map((link, index) => (
+          <li key={index} className='d-flex justify-content-center'>
+            <a href={link.url} className='py-2 my-2 display-5 fw-bold text-decoration-none'>
+              {link.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
-export default MobileMenu
+export default MobileMenu;
