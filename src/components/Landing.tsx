6bc91ebe9c7e49edbad8b5ccee64dfb23e4f016f
pabloarchuleta <pablo.archuleta@lcu.edu>
2025-07-24 11:07:21 -0500

    styled tag

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -3,9 +3,9 @@ import HeroImage from './HeroImage';
 
 const Landing: React.FC = () => {
   return (
-    <section className=''>
+    <section>
       <div className='container py-5 mt-2'>
-        <p className='display-1 caprasimo-regular text-center'>Pablo's TechTide</p>
+        <p className='display-1 caprasimo-regular text-center TT-landing-tag mb-1'>Pablo's TechTide</p>
         <p className='text-center h4'>Frontend to backend, logic to launch — code that flows with purpose.</p>
       </div>
       <HeroImage />
